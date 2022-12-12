c3a5a1de4b8b2d09b0f56f22288cea39bd105312
Pavan <pavanpathaneni@gmail.com>
2022-12-12 14:37:20 +0530

    First Commit

---
+++ SINGLYLINKEDLIST.java
@@ -0,0 +1,98 @@
+class SINGLYLINKEDLIST{
+    Node head;
+    private int size;
+    SINGLYLINKEDLIST(){
+        this.size = 0;
+    }
+    class Node{
+        String data;
+        Node next;
+        Node(String data){
+            this.data = data;
+            this.next = null;
+            size++;//incrementing the size
+        }
+    }
+    //inserting data at the beginning of the linkedlist
+    public void addFirst(String data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    //inserting data at the end of the linkedlist
+    public void addLast(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next!=null)
+            temp = temp.next;
+        temp.next = newNode;
+        
+    }
+    //Displaying the data present in the linkedlist
+    public void printList(){
+        if(head == null){
+            System.out.println("List is empty");
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+    //Deleting a node from the linkedlist
+    public void deleteFirst(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        size--;
+        head = head.next;
+    }
+    //Deleting a node from the last
+    public void deleteLast(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        size--;
+        if(head.next==null){
+            head = null;
+            return;
+        }
+        Node secondLast = head;
+        Node lastNode = head.next;
+        while(lastNode.next != null){
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
+        }
+        
+        secondLast.next = null;
+    }
+    //Returns the size pf the array
+    public int getSize(){
+        return size;
+    }
+    public static void main(String args[]){
+        SINGLYLINKEDLIST list = new SINGLYLINKEDLIST();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+        list.addLast("list");
+        list.printList();
+        list.addFirst("This");
+        list.printList();
+        list.deleteFirst();
+        list.deleteLast();
+        list.printList();
+        System.out.println(list.getSize());
+    }
+}
